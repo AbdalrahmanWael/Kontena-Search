f3c05c4cf86db3abc4acff6ecc1cc881773c3960
Abdalrahman Wael <Rahmanywael9@gmail.com>
2024-09-19 20:40:45 +0300

    tailwind, FastHTML setup, fa

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fasthtml.common import *
 
-app, rt = fast_app()
+body_style = "max-width: 700px; background-color: lavender; font-family: 'Short Stack', cursive;"
+tailwind = Script(src="https://cdn.tailwindcss.com")
+font = Style("""@import url('https://fonts.googleapis.com/css2?family=Short+Stack&display=swap');""")
+app = FastHTML(hdrs=(tailwind, font))
 
-@rt("/")
+@app.get('/')
 def get():
-    return Titled("Kontena Search", P("Let's do this!"))
+    page = ( 
+    Title("Kontena"), 
+    Body(
+        Titled("Kontena Search", P("Let's do this!"), Div(Img(src="Kontena.svg")),
+            Div(
+               Form(Input(type="text", name="SearchInput", placeholder="Search"), Button("Search", type="submit"))
+            )
+        ), cls='bg-gray-800', style=body_style)
+    )
+    return page 
+
+@app.get("/{fname:path}.{ext:static}")
+def static(fname:str, ext:str): return FileResponse(f'{fname}.{ext}')
 
 serve()
